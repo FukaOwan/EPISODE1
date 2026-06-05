d4d46c704b6e7a7d089354e8b4b13a7e8720b67d
FukaOwan <f.owan@clecom.jp>
2026-06-05 16:23:53 +0900

    unreviewed

--- shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -1,8 +1,21 @@
 package jp.co.sss.shop.controller.client.order;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class ClientOrderRegistController {
-
+	//届け先入力画面表示
+	@RequestMapping(path="/client/order/address/input",method=RequestMethod.POST)
+	public String showAddressInput() {
+	 return "order/address_input";
+ }
+ 	//支払方法選択画面表示
+	@RequestMapping(path="/client/order/payment/input", method=RequestMethod.POST)
+	public String showPaymentInput() {
+	 return "/client/order/check";
+ }
+	@RequestMapping(path="",method=Requeset)
+ 
 }
