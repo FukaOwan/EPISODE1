35eb302d50489232b92aaa285ab1470f4dfe747f
FukaOwan <f.owan@clecom.jp>
2026-06-08 17:11:11 +0900

    6/8

--- shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
+++ shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/basket/ClientBasketController.java
@@ -1,5 +1,43 @@
 package jp.co.sss.shop.controller.client.basket;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import jakarta.servlet.http.HttpSession;
+import jp.co.sss.shop.bean.BasketBean;
+import jp.co.sss.shop.form.ItemForm;
+
+@Controller
 public class ClientBasketController {
+	@RequestMapping(path="/client/basket/list", method=RequestMethod.GET)
+	public String listItem(ItemForm itemForm,HttpSession session){
+		
+		 List<BasketBean> basketList=( List<BasketBean>) session.getAttribute("basketList");
+		
+			if(basketList==null){
+				
+				 List<BasketBean> basketList1=new ArrayList<>();
+				 basketList= basketList1;
+					
+			}
+			BasketBean basketBean1=new BasketBean(1,"りんご",30);
+			basketList.add(basketBean1);
+			
+			
+	        session.setAttribute("basketBeans", basketList);
+	       
+			
+	
+		   return"/client/basket/list";
+		   
+}
+	@RequestMapping(path="/client/basket/list", method=RequestMethod.POST)
+	public String returnBasket() {
+		return "/client/basket/list";
+	}
 
 }
--- shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
+++ shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/order/ClientOrderRegistController.java
@@ -1,31 +1,138 @@
 package jp.co.sss.shop.controller.client.order;
 
+import java.util.List;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import jp.co.sss.shop.bean.BasketBean;
+import jp.co.sss.shop.bean.UserBean;
+import jp.co.sss.shop.entity.Item;
+import jp.co.sss.shop.entity.User;
+import jp.co.sss.shop.form.OrderForm;
+import jp.co.sss.shop.repository.ItemRepository;
+import jp.co.sss.shop.repository.OrderRepository;
+import jp.co.sss.shop.repository.UserRepository;
+
 @Controller
 public class ClientOrderRegistController {
+	@Autowired
+	OrderRepository orderRepository;
+	@Autowired
+	UserRepository userRepository;
+	@Autowired
+	ItemRepository itemRepository;
+	@Autowired
+	HttpSession session;
+	//注文入力フォーム情報初期化処理（届け先入力前に）
+		@RequestMapping(path="/client/order/address/input", method=RequestMethod.POST)
+		public String resetForm(OrderForm orderForm) {
+			Integer id  = (Integer) ((UserBean) session.getAttribute("user")).getId();
+			
+			User user = userRepository.findByIdAndDeleteFlag(id, 0);
+			
+			BeanUtils.copyProperties(user, orderForm);
+			orderForm.setPayMethod(1);
+			session.setAttribute("orderForm", orderForm);
+			return "redirect:/client/order/address/input";
+		}
+
 	//届け先入力画面表示
 	@RequestMapping(path="/client/order/address/input",method=RequestMethod.GET)
-	public String showAddressInput() {
+	public String showAddressInput(@Valid @ModelAttribute OrderForm orderForm, BindingResult result, HttpSession sessionForOrder, Model model) {
 		
-	 return "order/address_input";
+		model.addAttribute("orderForm", sessionForOrder.getAttribute("orderForm"));
+		/*if (result.hasErrors()) {
+			sessionForOrder.invalidate();
+		}*/
+		return "client/order/address_input";
+		
 	}
+	//届け先入力画面次へボタン押下時
+	@RequestMapping(path="/client/order/payment/input", method = RequestMethod.POST)
+	public String inputPayment(@Valid @ModelAttribute OrderForm orderForm,BindingResult result, HttpSession session) {
+		
+		
+		if(result.hasErrors()) {
+			 session.setAttribute("error", orderForm);
+			return "redirect:/client/order/address/input";
+		}else {
+			return "redirect:/client/order/payment/input";
+		}
+		
+	}
+	
+	
  	//支払方法選択画面表示
-	@RequestMapping(path="/client/order/payment/input", method=RequestMethod.POST)
-	public String showPaymentInput() {
-	 return "order/payment_input";
- }
-	//支払方法登録画面表示
-	@RequestMapping(path="/client/order/complete",method=RequestMethod.POST)
-	public String showOrderComplete() {
-		return "order/complete";
+	@RequestMapping(path="/client/order/payment/input", method=RequestMethod.GET)
+	public String showPaymentInput(HttpSession sessionForOrder, Model model) {
+		
+		model.addAttribute("orderInfo", sessionForOrder);
+		
+		return "client/order/payment_input";
 	}
-	//注文入力フォーム情報初期化処理（届け先入力前に）
-	@RequestMapping(path="/client/order/address/input", method=RequestMethod.POST)
-	public String resetForm() {
-		session
+	//支払方法選択画面次へボタン押下時
+	@RequestMapping(path="/client/order/check", method = RequestMethod.POST)
+	public String moveToCheck(OrderForm orderForm, HttpSession sessionForOrder, Integer payMethod) {
+		BeanUtils.copyProperties(sessionForOrder.getAttribute("orderForm"), orderForm);
+		orderForm.setPayMethod(payMethod);
+		
+		sessionForOrder.setAttribute("orderInfoVerPayMethod", orderForm);
+		return "redirect:/client/order/check";
+	}
+	
+	
+	//注文確認画面表示処理
+	@RequestMapping(path="/client/order/check", method=RequestMethod.GET)
+	public String showOrderCheck(HttpSession sessionForOrder, Model model) {
+		
+		List<BasketBean> basketLists = (List<BasketBean>) session.getAttribute("basketBeans");
+		
+		for (int i = 0; i <= basketLists.size() ;i++) {
+			Item item = itemRepository.findByIdAndDeleteFlag(basketLists.get(i).getId(), 0);
+		if (item.getStock() == 0){
+			model.addAttribute("errormessage");
+			//買い物かごから削除
+			basketLists.remove(i);
+			i--;
+			
+		}else if (basketLists.get(i).getStock() >= item.getStock()) {
+			model.addAttribute("errormessage");
+			//買い物かごを更新
+			BasketBean basketbean = new BasketBean();
+			BeanUtils.copyProperties(item, basketbean);
+			basketLists.set(i, basketbean);
+		}
+		session.setAttribute("basketInfo", basketLists);
+			
+		Integer sum = 0;
+		sum += basketLists.get(i).getStock() * item.getPrice();
+		model.addAttribute("orderSum", sum);
+		}
+		
+		return "client/order/check";
+	}
+	
+	//注文確認画面「戻る」押下時
+	@RequestMapping(path="/client/order/payment/back", method = RequestMethod.POST)
+	public String returnAddressInput() {
 		return "redirect:/client/order/address/input";
 	}
+	
+	//注文完了画面表示
+	@RequestMapping(path="/client/order/complete",method=RequestMethod.GET)
+	public String showOrderComplete() {
+		return "client/order/complete";
+	}
+	
+
+	
 }
