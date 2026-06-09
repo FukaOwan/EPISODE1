e797f62f8bddbf8680527d5663542658ce01cf16
ItoShingo <rika5178kari8729@gmail.com>
2026-06-09 13:33:57 +0900

    コントローラー編集

--- shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
+++ shared_shop_EPISODE1/src/main/java/jp/co/sss/shop/controller/client/item/ClientItemShowController.java
@@ -1,9 +1,12 @@
 package jp.co.sss.shop.controller.client.item;
 
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -43,20 +46,32 @@ public class ClientItemShowController {
 		return "index";
 	}
 	
-	@RequestMapping(path = "/client/item/detail/{id}", method = { RequestMethod.GET, RequestMethod.POST })
+	/**
+	 * 商品詳細画面 表示処理
+	 *
+	 * @param model    Viewとの値受渡し
+	 * @return "client/item/detail" 商品詳細画面
+	 */
+	@GetMapping("/client/item/detail/{id}")
 	public String itemDetail(@PathVariable Integer id,Model model) {
 		Item item = itemRepository.getReferenceById(id);
 		
 		ItemBean itemBean = new ItemBean();
-		itemBean.setImage(item.getImage());
-		itemBean.setName(item.getName());
-		itemBean.setPrice(item.getPrice());
-		itemBean.setStock(item.getStock());
-		itemBean.setCategoryName(item.getCategory().getName());
-		itemBean.setDescription(item.getDescription());
-		model.addAttribute("item",itemBean);
+		BeanUtils.copyProperties(item, itemBean);
 		
+		itemBean.setCategoryName(item.getCategory().getName());
+		
+		model.addAttribute("item",itemBean);
 		return "client/item/detail";
 	}
-
+	
+	/**
+	 * 商品一覧画面 表示処理（「戻るボタン」を機能させるための仮）
+	 *
+	 * @return "/client/item/list" 商品一覧画面
+	 */
+	@PostMapping(path = "/client/item/list/1")
+	public String itemList(){
+		return "/client/item/list";
+	}
 }
